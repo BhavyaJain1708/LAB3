942e09e517209a197f01e311b12a4c524b080010
BhavyaJain1708 <143415793+BhavyaJain1708@users.noreply.github.com>
2023-11-02 22:54:59 +0530

    Add files via upload

---
+++ Palindrome Number.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+	int i,n,t,k,s,N;
+	printf("Enter the number to be reversed");
+	scanf("%d",&N);
+	scanf("%d",&n);
+	k=N;
+	s=0;
+	for(i=0;N!=0;i++)
+	{
+		t=N%10;
+		N=N/10;
+		s = s + t*(pow(10,n-i-1));
+	}
+	printf("%d\n",s);
+	if(k==s)
+	{
+		printf("The given number is a Palindrome");
+	}
+	else
+	{
+		printf("The given number is not a Palindrome");
+	}
+	return 0;
+}
